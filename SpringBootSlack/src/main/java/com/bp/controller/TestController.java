a511e43e820e993f0f9a370aa146b3e83ffcfb96
bp <bp@gmail.com>
2020-07-15 22:14:45 +0530

    push code

---
+++ SpringBootSlack/src/main/java/com/bp/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello Mr. Bp !!!";
+	}
+}
